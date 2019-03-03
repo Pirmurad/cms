6b3fd060460d1700f32d9d0ca1cba53eeabccfe8
Pirmurad <zurou@outlook.com>
2019-03-02 21:47:04 -1200

    viewFolder və subViewFolder sturukturu quruldu.view qovluğunun içərisində list,add,update təməl alt qovluğlar yaradıldı. Məhsulların view hissəsini qurulması üçün giriş yaradıldı...

--- panel/application/controllers/Dashboard.php
+++ panel/application/controllers/Dashboard.php
@@ -8,13 +8,20 @@ class Dashboard extends CI_Controller {
 	public  function  __construct()
     {
         parent::__construct();
-        $this ->viewFolder = "dashboard_v";
+
+        $this->viewFolder =  "dashboard_v";
 
     }
 
 
     public function index()
 	{
-		$this->load->view("{$this->viewFolder}/index.php");
+	    $viewData = new stdClass();
+	    $viewData->viewFolder = $this->viewFolder;
+	    $viewData->subViewFolder = "list";
+
+
+
+		$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index.php",$viewData);
 	}
 }
