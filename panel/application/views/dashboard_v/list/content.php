757ea3059b89f26cb92f78be91be7887d8c9c79c
Pirmurad <zurou@outlook.com>
2019-03-03 10:10:06 -1200

    məhsullar modolunun məhsul siyahısı üçün html şablon yaradıldı

--- panel/application/views/dashboard_v/list/content.php
+++ panel/application/views/dashboard_v/list/content.php
@@ -1,1 +1,36 @@
-Urun listesi kontent...
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Məhsulların siyahısı</h4>
+    </div>
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <table class="table table-hover table-striped">
+               <thead>
+                <tr>
+                    <th>#id</th>
+                    <th>url</th>
+                    <th>başlıq</th>
+                    <th>açıqlama</th>
+                    <th>vəziyyəti</th>
+                    <th>əməliyyat</th>
+                </tr>
+               </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>monitor</td>
+                        <td>Monitor</td>
+                        <td>Full HD samsung monitoru</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                            <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square"></i> Düzəlt</a>
+
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+
+</div>
