75d80ee618479358efa31ed6271265dfb9ac3850
Pirmurad <zurou@outlook.com>
2019-03-03 19:41:41 -1200

    Toggle button əlavə edildi. Yeni əlavə et buttonu əlavə edildi. Məhsulun olmaması barədə alert əlavə edildi.

--- panel/application/views/dashboard_v/list/content.php
+++ panel/application/views/dashboard_v/list/content.php
@@ -1,9 +1,18 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Məhsulların siyahısı</h4>
+        <h4 class="m-b-lg">
+
+            Məhsulların siyahısı
+
+            <a href="#" class="btn btn-outline btn-primary btn-xs pull-right"><i class="fa fa-plus"></i>Yeni əlavə et</a>
+
+        </h4>
     </div>
     <div class="col-md-12">
         <div class="widget p-lg">
+            <div class="alert alert-info text-center">
+                <p>Burada hər hansı bir məhsula ayıd qeydlər mövcud deyil ! Əlavə etmək üçün <a href="#">klik edin</a></p>
+            </div>
             <table class="table table-hover table-striped">
                <thead>
                 <tr>
@@ -21,7 +30,9 @@
                         <td>monitor</td>
                         <td>Monitor</td>
                         <td>Full HD samsung monitoru</td>
-                        <td>1/0</td>
+                        <td>
+                                <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
                         <td>
                             <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
                             <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square"></i> Düzəlt</a>
