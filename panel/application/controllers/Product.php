32421d9d52c9b439702c4ac9ca1a8f94ae75e56c
Pirmurad <zurou@outlook.com>
2019-03-03 20:28:31 -1200

    Məhsulların siyahısı verilənlər bazasından çəkildi və dinamikləşdirildi..Product_model kodlandı

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -10,14 +10,21 @@ class Product extends CI_Controller
         parent::__construct();
 
         $this->viewFolder="product_v";
+
+        $this->load->model("product_model");
     }
 
     public function index(){
 
         $viewData = new stdClass();
+
+        /*Cədvəldən məlumatların çəkilməsi*/
+        $items=$this->product_model->get_all();
+
+        /*View-a göndəriləcək dəyişənlərin set edilməsi*/
         $viewData->viewFolder =  $this->viewFolder;
         $viewData->subViewFolder ="list";
-
+        $viewData->items=$items;
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
     }
 
--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,61 @@
-salamlar
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">
+
+            Məhsulların siyahısı
+
+            <a href="#" class="btn btn-outline btn-primary btn-xs pull-right"><i class="fa fa-plus"></i>Yeni əlavə et</a>
+
+        </h4>
+    </div>
+    <div class="col-md-12">
+        <div class="widget p-lg">
+
+            <?php if(empty($items)) {?>
+
+            <div class="alert alert-info text-center">
+                <p>Burada hər hansı bir məhsula ayıd qeydlər mövcud deyil ! Əlavə etmək üçün <a href="#">klik edin</a></p>
+            </div>
+            <?php } else { ?>
+            <table class="table table-hover table-striped">
+                <thead>
+                <tr>
+                    <th>#id</th>
+                    <th>url</th>
+                    <th>başlıq</th>
+                    <th>açıqlama</th>
+                    <th>vəziyyəti</th>
+                    <th>əməliyyat</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($items as $item) { ?>
+
+                    <tr>
+                        <td># <?php echo $item->id; ?></td>
+                        <td><?php echo $item->url; ?></td>
+                        <td><?php echo $item->title; ?></td>
+                        <td><?php echo $item->description; ?></td>
+                        <td>
+                            <input
+                                    type="checkbox"
+                                    data-switchery
+                                    data-color="#10c469"
+                                    <?php echo ($item->isActive) ? "checked":""; ?>
+                            />
+                        </td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                            <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square"></i> Düzəlt</a>
+                        </td>
+                    </tr>
+
+                <?php } ?>
+
+                </tbody>
+            </table>
+            <?php } ?>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+
+</div>
